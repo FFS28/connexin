ceb34576f032317e2d0bff8b1d4090feb1342a3b
Theo Edward <rasulovasliddin245@gmail.com>
2022-09-14 09:30:42 -0700

    fix

--- src/components/subPages/Admin/ManageUser/SetupUser.admin.tsx
+++ src/components/subPages/Admin/ManageUser/SetupUser.admin.tsx
@@ -152,7 +152,7 @@ export default function SetupUser({editData}: {editData : any}){
             setRole(editData.role)
             setActive(editData.active)
         }
-    }, [appState.editState, editData.ref] )
+    }, [appState.editState] )
 
     return (
         <>       
